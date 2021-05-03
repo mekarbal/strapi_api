607829d120d994b84c2864412717a454c55b4d0f
Elmahdi KARBAL <56792446+mekarbal@users.noreply.github.com>
2021-05-03 15:50:15 +0000

    strapi pi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '79feeaac8c0bb643165b04752ed12255'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9b2fa99c-325f-4096-ad34-321768e4f695'
+};
